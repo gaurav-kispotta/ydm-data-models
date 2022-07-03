33c4e6caeb293fdd7091dec90d4861ed9a880867
Gaurav Kispotta <gaurav.kispotta@outlook.com>
2022-07-03 21:35:20 +0530

    Using new module resolution library to fix this issue

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
-export { YdmGlobalConfig } from '@ydm-data-models/ydm-models/v1';
-export { YdmDashboard } from './src/models/ydm/v1/YdmDashboard';
-export { YdmVideoModel } from './src/models/ydm/v1/YdmVideoModel';
-export { YdmChannelModel } from './src/models/ydm/v1/YdmChannelModel';
+require('module-alias/register');
 
-export { YoutubeGlobalConfig } from './src/models/youtube-api/v3/YoutubeGlobalConfig';
-export { YoutubeChannelResponse } from './src/models/youtube-api/v3/YoutubeChannelResponse';
-export { YoutubeVideoResponse } from './src/models/youtube-api/v3/YoutubeVideoResponse';
+export { YdmGlobalConfig, YdmDashboard, YdmVideoModel, YdmChannelModel } from '@ydm-data-models/ydm-models/v1';
+
+export { YoutubeGlobalConfig, YoutubeChannelResponse, YoutubeVideoResponse } from '@ydm-data-models/youtube-api/v3';
