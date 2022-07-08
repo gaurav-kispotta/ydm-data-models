f01d6537224d91d716fab86390e80032e95f5577
Gaurav Kispotta <gaurav.kispotta@outlook.com>
2022-07-08 11:35:46 +0530

    Fixed TS compiler issue with path alias

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 require('module-alias/register');
 
-export { YdmGlobalConfig, YdmDashboard, YdmVideoModel, YdmChannelModel } from '@ydm-data-models/ydm-models/v1';
+export { YdmAppGlobalConfig, YdmDashboard, YdmVideoModel, YdmChannelModel } from '@ydm-data-models/ydm-models/v1';
 
 export { YoutubeGlobalConfig, YoutubeChannelResponse, YoutubeVideoResponse } from '@ydm-data-models/youtube-api/v3';
